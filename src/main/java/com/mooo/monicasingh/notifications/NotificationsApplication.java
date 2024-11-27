f9da9a64c368d2ebcb35b538b34711dfdc837c70
Waquar Shamsi <waquarshamsi@outlook.com>
2024-11-27 06:44:12 +0530

    initial project setup

---
+++ src/main/java/com/mooo/monicasingh/notifications/NotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.mooo.monicasingh.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsApplication.class, args);
+	}
+
+}
